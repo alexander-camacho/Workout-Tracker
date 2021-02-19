f1eda28c06e50ba49e948b9ae088ab3daf03fc9a
Alex Camacho <alsbrain@optonline.net>
2021-02-18 16:27:45 -0800

    updated routes and server file

---
+++ routes/apiRoutes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const db = require("../models");
+
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+  db.Workout.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+
+module.exports = router;
