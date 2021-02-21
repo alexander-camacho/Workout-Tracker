de6a7d7f406d610d8213cb5032593ad3e4e6bf0e
Alex Camacho <alsbrain@optonline.net>
2021-02-20 16:46:07 -0800

    fixing heroku credentials

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,18 +3,18 @@ const db = require("../models");
 
 router.get("/api/workouts", (req, res) => {
   db.Workout.find({})
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
 
-router.post("/api/workouts", ({ body }, res) => {
-  db.Workout.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
